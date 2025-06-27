68fd765ba0e0d88b52a79eee64d984bb514db017
Nintendosss <69327858+Nintendosss@users.noreply.github.com>
2025-06-27 14:33:05 -0300

    Revert

--- CustomJS.js
+++ CustomJS.js
@@ -24,45 +24,21 @@ waitForElement(['body'], ([body]) => {
 
   const backgroundContainer = document.createElement('div');
   backgroundContainer.className = 'starrynight-bg-container';
-
-
-  const hour = new Date().getHours();
-  let gradient;
-
-  if (hour >= 6 && hour < 12) {
-
-    gradient = 'linear-gradient(100deg, #1b2735 0%, #090a0f 100%)';
-  } else if (hour >= 12 && hour < 18) {
-   
-    gradient = 'linear-gradient(120deg, #1b2735 0%, #090a0f 100%)';
-  } else if (hour >= 18 && hour < 20) {
-
-    gradient = 'linear-gradient(150deg, #1b2735 0%, #090a0f 100%)';
-  } else {
-    gradient = 'linear-gradient(180deg, #1b2735 0%, #090a0f 100%)';
-  }
-
-  backgroundContainer.style.background = gradient;
-  backgroundContainer.style.transition = 'background 2s ease';
-
   body.appendChild(backgroundContainer);
 
-  // Adiciona estrelas
   const canvasSize = window.innerWidth * window.innerHeight;
   const starsFraction = canvasSize / 4000;
   for (let i = 0; i < starsFraction; i++) {
     const size = Math.random() < 0.5 ? 1 : 2;
     const star = document.createElement('div');
 
-    star.style.position = 'absolute';
     star.style.left = `${random(0, 99)}%`;
     star.style.top = `${random(0, 99)}%`;
     star.style.opacity = random(0.5, 1);
     star.style.width = `${size}px`;
     star.style.height = `${size}px`;
-    star.style.background = 'white';
-    star.style.borderRadius = '50%';
 
+  
     if (Math.random() < 0.2) {
       const animNum = Math.floor(Math.random() * 4) + 1;
       star.style.setProperty("animation", `twinkle${animNum} 5s infinite`, "important");
@@ -71,7 +47,6 @@ waitForElement(['body'], ([body]) => {
     backgroundContainer.appendChild(star);
   }
 
-  // Adiciona estrelas cadentes
   for (let i = 0; i < 4; i++) {
     const shootingstar = document.createElement('span');
     shootingstar.className = 'shootingstar';
@@ -84,17 +59,8 @@ waitForElement(['body'], ([body]) => {
       shootingstar.style.right = '-4px';
     }
 
-    shootingstar.style.position = 'absolute';
-    shootingstar.style.width = '2px';
-    shootingstar.style.height = '80px';
-    shootingstar.style.background = 'linear-gradient(white, transparent)';
-    shootingstar.style.borderRadius = '2px';
-    shootingstar.style.transform = 'rotate(45deg)';
-    shootingstar.style.animationName = 'shoot';
-    shootingstar.style.animationTimingFunction = 'ease-out';
     shootingstar.style.animationDuration = `${Math.floor(Math.random() * 3) + 3}s`;
     shootingstar.style.animationDelay = `${Math.floor(Math.random() * 7)}s`;
-    shootingstar.style.animationIterationCount = 'infinite';
 
     backgroundContainer.appendChild(shootingstar);
 
